f6fcf3ce33aecb9922b28419bf6f88c4d2f660a3
HeShiLie <gzben01@gmail.com>
2024-10-30 14:57:48 +0800

    Refactor SolarCLIPImageEmbedder module and add projection options
    This commit refactors the SolarCLIPImageEmbedder module in the encoders/modules.py file. It introduces the ability to choose different projection types for the image embeddings, including "ConvTrans" and "Linear". The "ConvTrans" projection uses a series of transposed convolutional layers, while the "Linear" projection uses a linear layer followed by reshaping. This update allows for more flexibility in generating image embeddings.

--- models/reconmodels/ldm/modules/encoders/modules.py
+++ models/reconmodels/ldm/modules/encoders/modules.py
@@ -1,38 +1,91 @@
-import torch
 import torch.nn as nn
-from functools import partial
-import clip
-from einops import rearrange, repeat
-# import kornia
+from einops import rearrange
+
+from models.reconmodels.ldm.util import instantiate_from_config
 
 
-class FrozenCLIPTextEmbedder(nn.Module):
-    """
-    Uses the CLIP transformer encoder for text.
-    """
-    def __init__(self, version='ViT-L/14', device="cuda", max_length=77, n_repeat=1, normalize=True):
+def disabled_train(self, mode=True):
+    """Overwrite model.train with this function to make sure train/eval mode
+    does not change anymore."""
+    return self
+
+class ReshapeTo2D(nn.Module):
+    def __init__(self, h=16, w=16):
         super().__init__()
-        self.model, _ = clip.load(version, jit=False, device="cpu")
-        self.device = device
-        self.max_length = max_length
-        self.n_repeat = n_repeat
-        self.normalize = normalize
+        self.h = h
+        self.w = w
 
-    def freeze(self):
-        self.model = self.model.eval()
-        for param in self.parameters():
-            param.requires_grad = False
+    def forward(self, x):
+        return rearrange(x, 'b c (h w) -> b c h w', h=self.h, w=self.w)
+    
+class LinearProjectionToImage(nn.Module):
+    def __init__(self, input_dim=(768, 16, 16), output_dim=(3, 64, 64)):
+        super(LinearProjectionToImage, self).__init__()
+        input_size = input_dim[0] * input_dim[1] * input_dim[2]  # 768 * 16 * 16
+        output_size = output_dim[0] * output_dim[1] * output_dim[2]  # 3 * 64 * 64
+        self.fc = nn.Linear(input_size, output_size)
+        self.activation = nn.Tanh()
 
-    def forward(self, text):
-        tokens = clip.tokenize(text).to(self.device)
-        z = self.model.encode_text(tokens)
-        if self.normalize:
-            z = z / torch.linalg.norm(z, dim=1, keepdim=True)
-        return z
+    def forward(self, x):
+        x = x.view(x.size(0), -1)  
+        x = self.fc(x)             
+        x = self.activation(x)      
+        return x.view(x.size(0), 3, 64, 64)  
 
-    def encode(self, text):
-        z = self(text)
-        if z.ndim==2:
-            z = z[:, None, :]
-        z = repeat(z, 'b 1 d -> b k d', k=self.n_repeat)
-        return z
+#-*************************************-#
+
+class SolarCLIPImageEmbedder(nn.Module):
+
+    def __init__(self, 
+                 solarclip_config,
+                 embedding_key='magnet_image', 
+                 projection_type='Linear', 
+                 **kwargs):
+        super().__init__()
+        self.projection_type = projection_type
+        self.embedding_key = embedding_key
+
+        self._init_solarclip(solarclip_config)
+        self.ReshapeProjection = self._get_projection(projection_type)
+
+    def _init_solarclip(self, solarclip_config, freeze=True):
+        solarclip = instantiate_from_config(solarclip_config)
+        if freeze:
+            self.solarclip = solarclip.eval()
+            self.solarclip.train = disabled_train
+            for param in self.solarclip.parameters():
+                param.requires_grad = False
+        if self.embedding_key == 'magnet_image':
+            self.solarclip = self.solarclip.visual_hmi
+        elif self.embedding_key == 'aia0094_image':
+            self.solarclip = self.solarclip.visual_aia
+        else:
+            raise ValueError(f"Unknown embedding key {self.embedding_key}")
+        
+    def _get_projection(self, projection_type):
+        projectin_options = {
+            "ConvTrans": nn.Sequential(
+                ReshapeTo2D(16, 16),
+                nn.ConvTranspose2d(768, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+                nn.GroupNorm(16, 128),
+                nn.Tanh(),
+                nn.ConvTranspose2d(128, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+                nn.GroupNorm(1, 3),
+                nn.Tanh()
+                ),
+            "Linear": nn.Sequential(
+                ReshapeTo2D(16, 16),
+                LinearProjectionToImage(input_dim=(768, 16, 16), output_dim=(3, 64, 64))
+                )
+        }
+
+        if projection_type in projectin_options:
+            return projectin_options[projection_type]
+        else:
+            raise ValueError(f"Unknown projection type {projection_type}")
+
+    def forward(self, x):
+        x = self.solarclip(x)
+        x = self.ReshapeProjection(x)
+        return x
+        
