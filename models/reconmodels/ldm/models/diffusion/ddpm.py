068e989f7ffcf44fd4ac1e3c20abe709cc35c7eb
ctf <chaitaifeng@163.com>
2024-10-30 19:57:56 +0800

    add config. and class SolarCLIPConditionedLatentDiffusionV2 in ddpm.py

--- models/reconmodels/ldm/models/diffusion/ddpm.py
+++ models/reconmodels/ldm/models/diffusion/ddpm.py
@@ -1480,3 +1480,32 @@ class SolarCLIPConditionedLatentDiffusion(LatentDiffusion):
             x_samples_cfg = self.decode_first_stage(samples_cfg)
             log[f"samplescfg_scale_{unconditional_guidance_scale:.2f}"] = x_samples_cfg
         return log
+    
+class SolarCLIPConditionedLatentDiffusionV2(LatentDiffusion):
+    def __init__(self):
+        super().__init__()
+        
+    def instantiate_cond_stage(self, config):
+        if not self.cond_stage_trainable:
+            if config == "__is_first_stage__":
+                print("Using first stage also as cond stage.")
+                self.cond_stage_model = self.first_stage_model
+            elif config == "__is_unconditional__":
+                print(f"Training {self.__class__.__name__} as an unconditional model.")
+                self.cond_stage_model = None
+                # self.be_unconditional = True
+            else:
+                model = instantiate_from_config(config)
+                self.cond_stage_model = model.eval()
+                self.cond_stage_model.train = disabled_train
+                for param in self.cond_stage_model.parameters():
+                    param.requires_grad = False
+        else:
+            assert config != '__is_first_stage__'
+            assert config != '__is_unconditional__'
+            model = instantiate_from_config(config)
+            self.cond_stage_model = model
+            self.cond_stage_model.solarclip = model.solarclip.eval()
+            self.cond_stage_model.solarclip.train = disabled_train
+            for param in self.cond_stage_model.solarclip.parameters():
+                param.requires_grad = False
