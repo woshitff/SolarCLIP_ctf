86322ed985eb3cb219fd8412c118171ef4105e3c
ctf <chaitaifeng@163.com>
2024-11-01 01:05:12 +0800

    添加VAE损失配置，并在LPIPS损失实现中新增参数支持和感知损失计算方式的调整。

--- models/reconmodels/autoencoder/modules/losses/contperceptual.py
+++ models/reconmodels/autoencoder/modules/losses/contperceptual.py
@@ -1,17 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import lpips
 
 class LPIPS(nn.Module):
+    # Learned perceptual image patch similarity (LPIPS) loss
     # see https://github.com/CompVis/stable-diffusion/blob/main/ldm/modules/losses/contperceptual.py
     def __init__(self, 
+                 rec_loss_type='l2',
+                 log_var_init=0.0,
                  kl_weight=0.1,
                  perceptual_weight=1):
         super().__init__()
+        self.rec_loss_type = rec_loss_type
         self.kl_weight = kl_weight
         self.perceptual_weight = perceptual_weight
 
+        self.logvar = nn.Parameter(torch.ones(size=())*log_var_init)
+        self.perceptual_loss = lpips.LPIPS(net='vgg').eval()
 
+        
     def forward(self, inputs, recons, posteriors, weights=None, log_split="train"):
 
         if self.rec_loss_type == 'l1':
@@ -22,7 +30,7 @@ class LPIPS(nn.Module):
             raise ValueError(f"Invalid rec_loss_type: {self.rec_loss_type}")
         
         if self.perceptual_weight > 0:
-            perceptual_loss = self.perceptual_loss(inputs.contiguous(), recons.contiguous())
+            perceptual_loss = self.perceptual_loss(inputs.repeat(1, 3, 1, 1).contiguous(), recons.repeat(1, 3, 1, 1).contiguous())
             rec_loss = rec_loss + self.perceptual_weight * perceptual_loss
 
         nll_loss = rec_loss / torch.exp(self.logvar) + self.logvar
@@ -36,6 +44,8 @@ class LPIPS(nn.Module):
             post_mu, post_logvar = posteriors
             kl_loss = -0.5 * torch.sum(1 + post_logvar - post_mu.pow(2) - post_logvar.exp())
             kl_loss = kl_loss / self.mu.shape[0]
+        else:
+            kl_loss = torch.tensor(0.0)
         
         loss = weighted_nll_loss + self.kl_weight * kl_loss
 
