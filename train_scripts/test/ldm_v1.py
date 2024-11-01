ba53067670efa27d1bb805dc2c3da7e67fe94247
HeShiLie <gzben01@gmail.com>
2024-11-01 13:39:14 +0800

    change config for 0094 to 0094

--- train_scripts/test/ldm_v1.py
+++ train_scripts/test/ldm_v1.py
@@ -45,7 +45,7 @@ def get_parser(**parser_kwargs):
         metavar="/home/chaitf/桌面/SolarCLIP/SolarCLIP_v2/configs/train_configs/reconmodels/ldm/test.yaml",
         help="paths to base configs. Loaded from left-to-right. "
              "Parameters can be overwritten or added with command-line options of the form `--key value`.",
-        default=["configs/train_configs/reconmodels/ldm/hmi2aia0094/vae_condition/0094aia2aia0094_concat_test.yaml"],
+        default=["configs/train_configs/reconmodels/ldm/hmi2aia0094/vae_condition/0094aia2aia0094_test_v0_concat.yaml"],
     )
     parser.add_argument(
         "-f",
