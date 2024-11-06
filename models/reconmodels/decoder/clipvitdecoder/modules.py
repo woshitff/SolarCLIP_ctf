a017a9429a144b8a24790b3445f44f8f002d63d2
2a40 <chaitaifeng@163.com>
2024-11-06 14:29:39 +0800

    refactor downsample code

--- models/reconmodels/decoder/clipvitdecoder/modules.py
+++ models/reconmodels/decoder/clipvitdecoder/modules.py
@@ -1,39 +1,29 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+import pytorch_lightning as pl
 
-class ImageResizeTarget(nn.Module):
-    def __init__(self, out_size, scale_factor=None):
+class ImageDownsample(nn.Module):
+    def __init__(self, in_size, scale_factor=None):
         """
         Args:
             out_size (int or tuple)
             scale_factor (float or int, optional)
         """
-        super(ImageResizeTarget, self).__init__()
-        self.out_size = out_size
+        super().__init__()
+        self.in_size = in_size
         self.scale_factor = scale_factor
-    
-    def forward(self, batch, k):
-        if k == 'magnet_image':
-            x = batch[:, 0, :, :, :]
-        elif k == 'aia0094_image':
-            x = batch[:, 1, :, :, :]
-        else:
-            raise NotImplementedError(f"Key {k} not supported")
-        
-        if len(x.shape) == 3:
-            x = x[..., None]
-        
         if self.scale_factor is not None:
-            new_height = int(x.shape[2] // self.scale_factor)
-            new_width = int(x.shape[3] // self.scale_factor)
-            target_size = (new_height, new_width)
+            self.out_size = tuple(int(dim * self.scale_factor) for dim in self.in_size)
         else:
-            target_size = self.out_size
+            self.out_size = self.in_size
+        assert isinstance(self.out_size, int), "Output size must be an integer"
 
-        resize_transform = transforms.Resize(target_size)
-        x = resize_transform(x)
-        
+    def downsample(self, x):
+        return nn.functional.interpolate(x, scale_factor=self.scale_factor, mode='bilinear', align_corners=False)
+
+    def forward(self, x):
+        assert len(x.shape) == 4, "Input tensor must have 4 dimensions, it is an image tensor"
+        x = self.downsample(x)
         x = x.to(memory_format=torch.contiguous_format).float()
-
         return x
