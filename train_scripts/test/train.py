90d4975ed40c75efbeda3ec4c065770edd42a5de
ctf <chaitaifeng@163.com>
2024-11-07 21:49:23 +0800

    更新配置文件名并修改训练脚本中的默认配置路径

--- train_scripts/test/train.py
+++ train_scripts/test/train.py
@@ -45,7 +45,7 @@ def get_parser(**parser_kwargs):
         metavar="/home/chaitf/桌面/SolarCLIP/SolarCLIP_v2/configs/train_configs/reconmodels/ldm/test.yaml",
         help="paths to base configs. Loaded from left-to-right. "
              "Parameters can be overwritten or added with command-line options of the form `--key value`.",
-        default=["configs/train_configs/reconmodels/decoder/clipvitdecoder/learnable_condition_diffusion_decoder/aia0094_cliptoken_to_aia0094_img_lr_x8.yaml"],
+        default=["configs/train_configs/reconmodels/ldm/mainconfig/diffusiondecoder/aia0094/aia0094_cliptoken_crossattn_1x16x16_ddpm_openaimodel.yaml"],
     )
     parser.add_argument(
         "-f",
