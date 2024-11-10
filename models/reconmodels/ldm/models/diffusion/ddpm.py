6f55ae97c452c1ce161baf0ea14b01368458f5bf
ctf_zjlab106_safe <chaitaifeng@163.com>
2024-11-10 11:18:59 +0800

    can run but need to check log images

--- models/reconmodels/ldm/models/diffusion/ddpm.py
+++ models/reconmodels/ldm/models/diffusion/ddpm.py
@@ -415,9 +415,11 @@ class DDPM(pl.LightningModule):
 
     def p_losses(self, x_start, t, noise=None):
         noise = default(noise, lambda: torch.randn_like(x_start))
+        print(f'noise: {noise.shape}')
         x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        print(f'x_nosiy {x_noisy.shape}')
         model_out = self.model(x_noisy, t)
-
+        print(f'model_out: {model_out.shape}')
         loss_dict = {}
         if self.parameterization == "eps":
             target = noise
@@ -428,7 +430,7 @@ class DDPM(pl.LightningModule):
         else:
             raise NotImplementedError(f"Parameterization {self.parameterization} not yet supported")
 
-        loss = self.get_loss(model_out, target, mean=False).mean(dim=[1, 2, 3])
+        loss = self.get_loss(model_out, target, mean=False).mean(dim=tuple(range(1, model_out.dim())))
 
         log_prefix = 'train' if self.training else 'val'
 
@@ -441,12 +443,13 @@ class DDPM(pl.LightningModule):
         loss = loss_simple + self.original_elbo_weight * loss_vlb
 
         loss_dict.update({f'{log_prefix}/loss': loss})
-
+        print(f'loss: {loss}')
         return loss, loss_dict
 
     def forward(self, x, *args, **kwargs):
         # b, c, h, w, device, img_size, = *x.shape, x.device, self.spatial_size
         # assert h == img_size and w == img_size, f'height and width of image must be {img_size}'
+        
         t = torch.randint(0, self.num_timesteps, (x.shape[0],), device=self.device).long()
         return self.p_losses(x, t, *args, **kwargs)
 
@@ -954,7 +957,7 @@ class LatentDiffusion(DDPM):
         else:
             raise NotImplementedError()
 
-        loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
+        loss_simple = self.get_loss(model_output, target, mean=False).mean(dim=tuple(range(1,model_output.dim())))
         loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
         
         logvar_t = self.logvar.to(self.device)[t]
@@ -966,7 +969,7 @@ class LatentDiffusion(DDPM):
 
         loss = self.l_simple_weight * loss.mean()
 
-        loss_vlb = self.get_loss(model_output, target, mean=False).mean(dim=(1, 2, 3))
+        loss_vlb = self.get_loss(model_output, target, mean=False).mean(dim=tuple(range(1,model_output.dim())))
         loss_vlb = (self.lvlb_weights[t] * loss_vlb).mean()
         loss_dict.update({f'{prefix}/loss_vlb': loss_vlb})
         loss += (self.original_elbo_weight * loss_vlb)
