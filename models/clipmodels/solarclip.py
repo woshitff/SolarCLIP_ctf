92224d0ab8e2fdf2d385ab38e9daa90c9c26e93d
ctf <chaitaifeng@163.com>
2024-11-11 00:40:36 +0800

    重构SolarCLIP类，整合基础配模型的编码方法，更新前向传播和损失计算逻辑，简化参数传递，增强训练与验证功能，并实现优化器配置。

--- models/clipmodels/solarclip.py
+++ models/clipmodels/solarclip.py
@@ -3,10 +3,11 @@ from typing import Tuple, Union
 import torch
 import torch.nn as nn
 import numpy as np
+import pytorch_lightning as pl
 
+from models.reconmodels.autoencoder.util import config_optimizers
 from models.clipmodels.modules.vit import VisionTransformer, Remove_class_token
 from models.reconmodels.ldm.util import instantiate_from_config
-
 
 def disabled_train(self, mode=True):
     """Overwrite model.train with this function to make sure train/eval mode
@@ -14,123 +15,112 @@ def disabled_train(self, mode=True):
     return self
 
 
-class SolarCLIP(nn.Module):
+class SolarCLIP(pl.LightningModule):
     def __init__(self,
-                 ckpt_path: str,
-                 embed_dim: int,
-                 vision_width: int,
-                 # mag vision
-                 image_resolution_hmi: int,
-                 vision_layers_hmi: Union[Tuple[int, int, int, int], int],
-                 vision_patch_size_hmi: int,
-                 # 11 channels
-                 image_resolution_aia: int,
-                 vision_layers_aia: Union[Tuple[int, int, int, int], int],
-                 vision_patch_size_aia: int,
-                 # loss token type
-                 transformer_token_type: str,
-                 norm_type: str 
-                 ):
+                 ckpt_path: str=None,
+                 base_modal_key: str='hmi_image',
+                 base_modal_config: dict=None,
+                 paired_modal_key: str='aia0094_image',
+                 paired_modal_config: dict=None,
+                 token_type: str='all embedding',
+                 inner_loss_rate: float=0.0,
+                         ):
         super().__init__()
-        # self.save_hyperparameters()
+        self.save_hyperparameters()
+        self.base_modal_key = base_modal_key
+        self.paired_modal_key = paired_modal_key
+        self.token_type = token_type
+        self.inner_loss_rate = inner_loss_rate
 
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
-        self.transformer_token_type = transformer_token_type
-
-        vision_heads = vision_width // 64
-        self.visual_hmi = VisionTransformer(
-                in_channels=1,
-                input_resolution=image_resolution_hmi,
-                patch_size=vision_patch_size_hmi,
-                width=vision_width,
-                layers=vision_layers_hmi,
-                heads=vision_heads,
-                output_dim=embed_dim,
-                token_type = transformer_token_type,
-                norm_type= norm_type
-            )
-        
-        self.visual_aia = VisionTransformer(
-                in_channels=1,
-                input_resolution=image_resolution_aia,
-                patch_size=vision_patch_size_aia,
-                width=vision_width,
-                layers=vision_layers_aia,
-                heads=vision_heads,
-                output_dim=embed_dim,
-                token_type = transformer_token_type,
-                norm_type = norm_type
-            )
+        self.instantiate_basemodal_encode(base_modal_config)
+        self.instantiate_pairedmodal_encode(paired_modal_config)
 
         if ckpt_path is not None:
             self.init_from_ckpt(ckpt_path)
 
-    def init_from_ckpt(self, ckpt_path: str):
-        ckpt = torch.load(ckpt_path)
-        self.load_state_dict(ckpt)
+    @torch.no_grad()
+    def init_from_ckpt(self, ckpt_path):
+        checkpoint = torch.load(ckpt_path)
+        self.load_state_dict(checkpoint, strict=False)
 
-    @property
-    def dtype(self):
-        # return self.visual_hmi.conv1.weight.dtype
-        return self.visual_hmi.conv1.conv1.weight.dtype
+    def instantiate_basemodal_encode(self, base_modal_config):
+        if self.base_model_key == 'hmi_image':
+            model = instantiate_from_config(base_modal_config)
+            self.base_model = model.eval()
+            self.base_model.train = disabled_train
+            for param in self.base_model.parameters():
+                param.requires_grad = False
+            self.encode_base = self.base_model.encode
+        else:
+            raise NotImplementedError(f"Base model key {self.base_model_key} not supported, please choose from ['hmi_image']")
+        
+    def instantiate_pairedmodal_encode(self, paired_modal_config):
+        model = instantiate_from_config(paired_modal_config)
+        if model.__class__.__name__ == 'ViTMAE':
+            self.encode_paired = model.encode
+        else:
+            self.encode_paired = model
 
-    def encode_hmi(self, image_hmi):
-        return self.visual_hmi(image_hmi.type(self.dtype))
-
-    def encode_aia(self, image_aia):
-        return self.visual_aia(image_aia.type(self.dtype))
-
-    def forward(self, image_hmi, image_aia, token_weight_1=None, token_weight_2=None):
-
-        hmi_features = self.encode_hmi(image_hmi)   #shape = [batch_size, length,embed_dim]
-        aia_features = self.encode_aia(image_aia)
+    def get_input(self, batch, k):
+        if k == 'hmi_image':
+            x = batch[:, 0, :, :, :]
+        elif k == 'aia0094_image':
+            x = batch[:, 1, :, :, :]
+        else:
+            raise NotImplementedError(f"Key {k} not supported")
+        if len(x.shape) == 3:
+            x = x[..., None]
+        x = x.to(memory_format=torch.contiguous_format).float()
+        return x
+    
+    def forward(self, x_base, x_paired, token_weight_base=None, token_weight_paired=None):
+        base_features = self.encode_base(x_base)   #shape = [batch_size, length,embed_dim]
+        paired_features = self.encode_paired(x_paired)
         
         # normalized features
-        hmi_features = hmi_features / (hmi_features.norm(dim=-1, keepdim=True)+1e-32)
-        aia_features = aia_features / (aia_features.norm(dim=-1, keepdim=True)+1e-32)
+        base_features = base_features / (base_features.norm(dim=-1, keepdim=True)+1e-32)
+        paired_features = paired_features / (paired_features.norm(dim=-1, keepdim=True)+1e-32)
 
         # cosine similarity as logits
         logit_scale = self.logit_scale.exp()
 
-        if self.transformer_token_type == 'class embedding':
-            logits_per_hmi = logit_scale * hmi_features @ aia_features.t()
-            logits_per_aia = logits_per_hmi.t()
+        if self.token_type == 'class embedding':
+            logits_per_base = logit_scale * base_features @ paired_features.t()
+            logits_per_paired = logits_per_base.t()
             inner_cor_matrix = None
 
-        elif self.transformer_token_type == 'all embedding':
-            B, L = hmi_features.shape[0], hmi_features.shape[1]
-            if token_weight_1 is None:
-                token_weight_1 = torch.ones([B,L],dtype = hmi_features.dtype,device = hmi_features.device)
-            if token_weight_2 is None:
-                token_weight_2 = torch.ones([B,L],dtype = aia_features.dtype,device = aia_features.device)
-            assert (token_weight_1.shape == (B, L) and token_weight_2.shape == (B, L)) # [B,L] tensor
-            token_weight_1 = token_weight_1.unsqueeze(-1)
-            token_weight_2 = token_weight_2.unsqueeze(-1)
+        elif self.token_type == 'all embedding':
+            B, L = base_features.shape[0], base_features.shape[1]
+            if token_weight_base is None:
+                token_weight_base = torch.ones([B,L],dtype = base_features.dtype,device = base_features.device)
+            if token_weight_paired is None:
+                token_weight_paired = torch.ones([B,L],dtype = paired_features.dtype,device = paired_features.device)
+            assert (token_weight_base.shape == (B, L) and token_weight_paired.shape == (B, L)) # [B,L] tensor
+            token_weight_base = token_weight_base.unsqueeze(-1)
+            token_weight_paired = token_weight_paired.unsqueeze(-1)
 
-            hmi_features = torch.einsum('BLD,BLd->BLD', hmi_features, token_weight_1)
-            aia_features = torch.einsum('BLD,BLd->BLD', aia_features, token_weight_2)
-            inner_cor_matrix = torch.einsum('BLD,BlD->BLl', hmi_features, aia_features)
+            base_features = torch.einsum('BLD,BLd->BLD', base_features, token_weight_base)
+            paired_features = torch.einsum('BLD,BLd->BLD', paired_features, token_weight_paired)
+            inner_cor_matrix = torch.einsum('BLD,BlD->BLl', base_features, paired_features)
             inner_cor_matrix = inner_cor_matrix.mean(dim=0) # [L,L]
-            cor_matrix = torch.einsum('BLD,bLD->BbL', hmi_features, aia_features)
+            cor_matrix = torch.einsum('BLD,bLD->BbL', base_features, paired_features)
             cor_matrix = cor_matrix.mean(dim=-1) # [B,B]
 
-            logits_per_hmi = logit_scale * cor_matrix
-            logits_per_aia = logits_per_hmi.t()
+            logits_per_base = logit_scale * cor_matrix
+            logits_per_paired = logits_per_base.t()
             inner_cor_matrix = logit_scale * inner_cor_matrix
 
-        # shape = [global_batch_size, global_batch_size]
-        # return logits_per_hmi, logits_per_H
-        return logits_per_hmi, logits_per_aia, inner_cor_matrix
-    
-    def calculate_loss(self, hmi_image, aia_image, inner_loss_rate = 0, token_weight_1 = None, token_weight_2 = None, criterion = torch.nn.functional.cross_entropy):
+        return logits_per_base, logits_per_paired, inner_cor_matrix
 
-        logits_per_hmi, logits_per_aia, inner_cor_matrix = self.forward(hmi_image, aia_image, token_weight_1, token_weight_2)
-        ground_truth = torch.arange(len(hmi_image), dtype=torch.long, device=hmi_image.device)
+    def loss_function(self, logits_per_base, logits_per_paired, inner_cor_matrix, inner_loss_rate = 0, token_weight_base = None, token_weight_paired = None, criterion = nn.functional.cross_entropy):
+
+        ground_truth = torch.arange(logits_per_base.shape[0], dtype=torch.long, device=logits_per_base.device)
         
-        loss_hmi = criterion(logits_per_hmi, ground_truth)
-        loss_aia = criterion(logits_per_aia, ground_truth)
-        loss = (loss_hmi + loss_aia) / 2
-        acc = (torch.argmax(logits_per_hmi, dim=1) == ground_truth).float().mean().item()
+        loss_base = criterion(logits_per_base, ground_truth)
+        loss_paired = criterion(logits_per_paired, ground_truth)
+        loss = (loss_base + loss_paired) / 2
+        acc = (torch.argmax(logits_per_base, dim=1) == ground_truth).float().mean().item()
 
         assert inner_loss_rate >=0
         if inner_loss_rate > 0:
@@ -141,7 +131,83 @@ class SolarCLIP(nn.Module):
         else:
             loss_inner = torch.tensor(0, dtype=torch.float32, device=inner_cor_matrix.device)
 
-        return loss, loss_inner, acc, logits_per_hmi, inner_cor_matrix
+        return loss, loss_inner, acc
+    
+    def get_loss(self, logits_per_base, logits_per_paired, inner_cor_matrix, inner_loss_rate = 0., token_weight_base = None, token_weight_paired = None, criterion = torch.nn.functional.cross_entropy):
+        log_prefix = 'train' if self.training else 'val'
+        loss_dict = {}
+
+        loss, loss_inner, acc = self.loss_function(logits_per_base, logits_per_paired, inner_cor_matrix, inner_loss_rate = inner_loss_rate, token_weight_base = token_weight_base, token_weight_paired = token_weight_paired, criterion = criterion)
+        loss_dict.update({f'{log_prefix}/loss': loss})
+        loss_dict.update({f'{log_prefix}/loss_inner': loss_inner})
+        loss_dict.update({f'{log_prefix}/acc': acc})
+        
+        return loss, loss_dict
+    
+    def shared_step(self, batch, batch_idx):
+        base_modal_data = self.get_input(batch, self.base_modal_key)
+        paired_modal_data = self.get_input(batch, self.paired_modal_key)
+
+        logits_per_base, logits_per_paired, inner_cor_matrix = self(base_modal_data, paired_modal_data, token_weight_base=None, token_weight_paired=None)
+
+        loss, loss_dict = self.get_loss(logits_per_base, logits_per_paired, inner_cor_matrix, inner_loss_rate = self.inner_loss_rate, token_weight_base = None, token_weight_paired = None, criterion = torch.nn.functional.cross_entropy)
+        return loss, loss_dict
+    
+    def training_step(self, batch, batch_idx):
+        loss, loss_dict = self.shared_step(batch, batch_idx)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log_dict(loss_dict, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, loss_dict = self.shared_step(batch, batch_idx)
+        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log_dict(loss_dict, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+    
+    def configure_optimizers(self):
+        lr = self.learning_rate
+        opt, scheduler = config_optimizers(self.learning_optimizer, self.parameters(), lr, self.learning_schedule)
+        return (opt, scheduler)
+    
+    def log_images(self, batch, N=2):
+        """
+        Log a batch of images to tensorboard and local
+
+        output:
+            log: dictionary of images to log
+            modals: dictionary of modalities determing the cmap and vmin/vmax for each image
+        """
+        log = dict()
+        modals = dict()
+
+        with torch.no_grad():
+            inputs = self.get_input(batch, self.input_modal_key)
+            targets = inputs.clone()
+            inputs = inputs.to(self.device)
+        N = min(N, inputs.shape[0])
+        log['inputs'] = inputs[:N]
+        log['targets'] = targets[:N]
+        self.eval()
+        with torch.no_grad():
+            targets_token_hat_mask_ratio_set = self(inputs, mask_ratio=self.mask_ratio)[0]
+            targets_hat_mask_ratio_set = self.unpatchify(targets_token_hat_mask_ratio_set)
+            targets_token_hat_mask_ratio_0 = self(inputs, mask_ratio=0.)[0]
+            targets_hat_mask_ratio_0 = self.unpatchify(targets_token_hat_mask_ratio_0)
+            targets_token_hat_inference = self.forward_inference(inputs)
+            targets_hat_inference = self.unpatchify(targets_token_hat_inference)
+        log['targets_hat_mask_ratio_set'] = targets_hat_mask_ratio_set[:N]
+        log['targets_hat_mask_ratio_0'] = targets_hat_mask_ratio_0[:N]
+        log['targets_hat_inference'] = targets_hat_inference[:N]
+        self.train()
+
+        modals['inputs'] = self.input_modal_key
+        modals['targets'] = self.input_modal_key
+        modals['targets_hat_mask_ratio_set'] = self.input_modal_key
+        modals['targets_hat_mask_ratio_0'] = self.input_modal_key
+        modals['targets_hat_inference'] = self.input_modal_key
+        print('image log done')
+        return log, modals
     
 
 class SolarCLIP_remove_CLS(nn.Module):
