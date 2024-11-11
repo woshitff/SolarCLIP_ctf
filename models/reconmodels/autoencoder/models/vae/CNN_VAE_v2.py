3abfc756e234c8a23103e22c667fdd72c2809eb2
zjlab106_nas-tq <chaitaifeng@163.com>
2024-11-11 15:28:00 +0800

    delete attention in cnn_vae_v2

--- models/reconmodels/autoencoder/models/vae/CNN_VAE_v2.py
+++ models/reconmodels/autoencoder/models/vae/CNN_VAE_v2.py
@@ -9,80 +9,10 @@ import pytorch_lightning as pl
 from models.reconmodels.autoencoder.util import config_optimizers
 from models.reconmodels.autoencoder.util import instantiate_from_config
 """
-SolarReconModel_VAE Model.
+SolarReconModel_VAE_v2 Model.
 This model use VAE architecture to reconstruct the solar image without clip model.
 """
 
-class SelfAttention(nn.Module):
-    def __init__(self, n_heads, d_embed, in_proj_bias = True, out_proj_bias= True):
-        super().__init__()
-        
-        #Combining the Wq, Wk, and Wv matrices into one
-        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias= in_proj_bias)
-        
-        #Represent the Wo Matrix
-        self.out_proj = nn.Linear(d_embed, d_embed, bias= out_proj_bias)
-        self.n_heads = n_heads
-        self.d_head = d_embed // n_heads
-        
-    def forward(self, x, causal_mask = False):
-        
-        input_shape = x.shape
-        
-        batch_size, sequence_length, d_embed = input_shape
-        
-        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
-        
-        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim * 3) -> 3 tensor of shape (Batch_Size, Seq_Len, Dim)
-        q, k ,v = self.in_proj(x).chunk(3, dim= -1)
-        
-        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, H, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
-        q = q.view(interim_shape).transpose(1,2)
-        k = k.view(interim_shape).transpose(1,2)
-        v = v.view(interim_shape).transpose(1,2)
-        
-        weight = q @ k.transpose(-1,-2)
-        
-        if causal_mask:     
-            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
-            weight.masked_fill_(mask, -torch.inf)
-        
-        weight /= math.sqrt(self.d_head)
-        
-        weight = F.softmax(weight, dim = -1)
-        
-        output = weight @ v
-        
-        output = output.transpose(1,2)
-        
-        output = output.reshape(input_shape)
-        
-        output = self.out_proj(output)
-        
-        return output
-
-class VAE_AttentionBlock(nn.Module):
-    def __init__(self, 
-                 channels, 
-                 num_groups: int = 32):
-        super().__init__()
-        self.groupnorm = nn.GroupNorm(num_groups, channels)
-        self.attention = SelfAttention(1,channels)
-        
-    def forward(self,x):
-        
-        residue = x
-        x = self.groupnorm(x)
-        
-        n, c, h, w = x.shape
-        x = x.view(n ,c,h *w)
-        x = x.permute(0,2,1)                
-        x = self.attention(x)
-        x = x.permute(0,2,1)        
-        x = x.view(n,c,h,w)
-    
-        x += residue
-        return x
     
 class VAE_ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, num_groups: int = 16,
