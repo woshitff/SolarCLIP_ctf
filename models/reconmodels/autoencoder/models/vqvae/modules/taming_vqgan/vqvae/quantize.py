fa2eeec5681dfbf1117139cb8af3c268533cf9a1
ctf_work_chaitf <chaitaifeng@163.com>
2024-11-12 21:59:10 +0800

    modify

--- models/reconmodels/autoencoder/models/vqvae/modules/taming_vqgan/vqvae/quantize.py
+++ models/reconmodels/autoencoder/models/vqvae/modules/taming_vqgan/vqvae/quantize.py
@@ -274,7 +274,7 @@ class VectorQuantizer2(nn.Module):
         assert return_logits==False, "Only for interface compatible with Gumbel"
         # reshape z -> (batch, height, width, channel) and flatten
         z = rearrange(z, 'b c h w -> b h w c').contiguous()
-        z_flattened = z.view(-1, self.e_dim)
+        z_flattened = z.view(-1, self.e_dim) 
         # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
 
         d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
