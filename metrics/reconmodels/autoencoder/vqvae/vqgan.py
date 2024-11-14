8aedc4e9692362fbc70dd4955bd87755c2256cd0
ctf_server <chaitaifeng@163.com>
2024-11-14 11:19:52 +0800

    去掉像转换中的 ToTensor

--- metrics/reconmodels/autoencoder/vqvae/vqgan.py
+++ metrics/reconmodels/autoencoder/vqvae/vqgan.py
@@ -11,7 +11,7 @@ class FID:
         self.transform = transforms.Compose([
         transforms.Resize(299),
         transforms.CenterCrop(299),
-        transforms.ToTensor(),
+        # transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
