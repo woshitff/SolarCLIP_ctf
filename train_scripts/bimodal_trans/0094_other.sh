50fe52308e958184f817ae5ea200a7d0d21a9bf6
zerolpls <2941655401@qq.com>
2025-03-31 15:46:00 +0800

    更新多个 bimodal 转换模型配置中的基础学习率

--- train_scripts/bimodal_trans/0094_other.sh
+++ train_scripts/bimodal_trans/0094_other.sh
@@ -1,10 +1,10 @@
 python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0131.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0171.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0193.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0211.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0304.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0355.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to1600.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to1700.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to4500.yaml
-python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094tohmi.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0171.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0193.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0211.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0304.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to0355.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to1600.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to1700.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094to4500.yaml
+# python train_scripts/train.py --base configs/train_configs/reconmodels/ldm/mainconfig/bimodal_trans/0094_other/0094tohmi.yaml
