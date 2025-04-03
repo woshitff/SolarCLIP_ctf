e76ae2cee807e6ac783fdfb5e3771c6cfb0c1a63
zerolpls <2941655401@qq.com>
2025-04-03 15:49:07 +0800

    修复加载检查点时模型状态字典的键访问错误

--- models/reconmodels/autoencoder/models/vae/CNN_VAE_v2.py
+++ models/reconmodels/autoencoder/models/vae/CNN_VAE_v2.py
@@ -645,7 +645,7 @@ class CNN_VAE_two(pl.LightningModule):
 
         if ckpt_path is not None:
             print('11111111111',self.state_dict().keys())
-            print('22222222222',torch.load(ckpt_path).keys())
+            print('22222222222',torch.load(ckpt_path)['model'].keys())
             self.init_from_ckpt(ckpt_path)
 
     def init_from_ckpt(self, ckpt_path):
