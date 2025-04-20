e53922bb65172b515ba68f4424f6b2b8545c4ab3
zjlab106_nas-tq <chaitaifeng@163.com>
2025-04-20 20:22:45 +0800

    no

--- train_scripts/ae/contrast.sh
+++ train_scripts/ae/contrast.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
-
+#!/bin/bash
+export MASTER_ADDR="localhost"  # 必须用 export 导出为环境变量
+export MASTER_PORT="29500"      # 同上
 python models/reconmodels/autoencoder/models/vae/JointVAE_train_v2.py
