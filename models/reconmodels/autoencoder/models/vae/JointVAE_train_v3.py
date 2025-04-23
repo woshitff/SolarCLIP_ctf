8198496448a4ff3b62f8760d105eeea92baaafd9
341_from_PC <838839085@qq.com>
2025-04-23 16:43:02 +0800

    no

--- models/reconmodels/autoencoder/models/vae/JointVAE_train_v3.py
+++ models/reconmodels/autoencoder/models/vae/JointVAE_train_v3.py
@@ -311,8 +311,9 @@ class MultiCheckpoint(ModelCheckpoint):
                     plt.close(rec_fig)
                     plt.close(latent_fig)
                 
-            if torch.distributed.is_initialized():
-                torch.distributed.barrier()
+        if torch.distributed.is_initialized():
+            torch.distributed.barrier()
+        print(f"Checkpoint saved to {file_path}")
                         
 
 
@@ -396,7 +397,7 @@ def train(config, opt):
     trainer = pl.Trainer(
         accelerator="gpu",
         # precision='bf16-mixed',
-        strategy='ddp_find_unused_parameters_true',
+        strategy='ddp_spawn_find_unused_parameters_true',
         max_epochs=epochs,
         logger=logger,
         callbacks=[checkpoint_callback],
