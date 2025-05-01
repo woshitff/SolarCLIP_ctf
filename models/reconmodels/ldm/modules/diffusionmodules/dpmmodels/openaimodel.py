4b90b86b19a629cfb65a6e6a8fe692662ce9a1df
zerolpls <2941655401@qq.com>
2025-05-01 21:22:26 +0800

    在 AttentionBlock 中添加调试输出以打印输入张量类型

--- models/reconmodels/ldm/modules/diffusionmodules/dpmmodels/openaimodel.py
+++ models/reconmodels/ldm/modules/diffusionmodules/dpmmodels/openaimodel.py
@@ -321,6 +321,7 @@ class AttentionBlock(nn.Module):
     def _forward(self, x):
         b, c, *spatial = x.shape
         x = x.reshape(b, c, -1)
+        print('xxxxxxxxxx',x.type())
         qkv = self.qkv(self.norm(x))
         h = self.attention(qkv)
         h = self.proj_out(h)
