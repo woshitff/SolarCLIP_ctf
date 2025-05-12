88041b8f5969a00b2690c5e4ddce3bc3976e17cb
zerolpls <2941655401@qq.com>
2025-05-12 15:43:25 +0800

    添加新的训练脚本 ldm0304_0094.sh，更新 ldm0094.sh 中的输出文件名，新增配置文件 ldm0094_concat_0304.yaml

--- train_scripts/ldm/ldm0094.sh
+++ train_scripts/ldm/ldm0094.sh
@@ -1,1 +1,1 @@
-python train_scripts/train.py > test2.out --b 'configs/train_configs/reconmodels/ldm/mainconfig/ldm_zyz/ldm0094_crossattn_3_openaiunt.yaml'
+python train_scripts/train.py > test3.out --b 'configs/train_configs/reconmodels/ldm/mainconfig/ldm_zyz/ldm0094_concat_0094.yaml'
